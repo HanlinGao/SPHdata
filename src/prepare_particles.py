d6d9be44240e74992716b8b39d1c56c57c6180cf
hanlin <hanlingao@outlook.com>
2021-02-10 14:47:18 -0500

    matplot visuialization for first timestep

--- src/prepare_particles.py
+++ src/prepare_particles.py
@@ -75,7 +75,20 @@ def obj_volume_to_particles(objpath, scale=1, radius=None):
         return numpy_from_bgeo(outpath)
 
 
-def prepare_particles(scene):
+def write_particles(path_without_ext, pos, vel=None):
+    """Writes the particles as point cloud ply.
+    Optionally writes particles as bgeo which also supports velocities.
+    """
+    arrs = {'pos': pos}
+    if not vel is None:
+        arrs['vel'] = vel
+    np.savez('../out_ply/' + path_without_ext + '.npz', **arrs)
+
+    pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(pos))
+    o3d.io.write_point_cloud('../out_ply/' + path_without_ext + '.ply', pcd)
+
+
+def prepare_particles(scene, output_dir):
     """generate initial data of box, box_normals, fluid_pos, fluid_vel according to the input json file
 
     :param scene: set path to box.obj and fluid.obj, as well as the translation of positions
@@ -91,6 +104,9 @@ def prepare_particles(scene):
         walls.append((points, normals))
     box = np.concatenate([x[0] for x in walls], axis=0)
     box_normals = np.concatenate([x[1] for x in walls], axis=0)
+    
+    # export static particles
+    write_particles(os.path.join(output_dir, 'box'), box, box_normals)
 
     # prepare fluids
     fluids = []
@@ -110,34 +126,54 @@ def prepare_particles(scene):
     for points, velocities, range_ in fluids:
         pos = np.concatenate([pos, points], axis=0)
         vel = np.concatenate([vel, velocities], axis=0)
+    
+    # export fluid particles
+    write_particles(os.path.join(output_dir, 'fluids'), pos, vel)
 
     return box, box_normals, pos, vel
 
 
-def visualize_prepared(prepared_box, prepared_fluids):
+def mat_visualize_prepared(prepared_box=np.empty(shape=(0, 3)), prepared_fluids=np.empty(shape=(0, 3))):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    box_x = prepared_box[:, 0]
-    box_y = prepared_box[:, 1]
-    box_z = prepared_box[:, 2]
-
-    fluid_x = prepared_fluids[:, 0]
-    fluid_y = prepared_fluids[:, 1]
-    fluid_z = prepared_fluids[:, 2]
-
-    ax.scatter(box_x, box_y, box_z, c='grey', marker='o', alpha=0.2)
-    ax.scatter(fluid_x, fluid_y, fluid_z, c='r', marker='o', alpha=0.7)
+    if len(prepared_box):
+        box_x = prepared_box[:, 0]
+        box_y = prepared_box[:, 1]
+        box_z = prepared_box[:, 2]
+        ax.scatter(box_x, box_z, box_y, c='whitesmoke', marker='o', alpha=0.1)
+    if len(prepared_fluids):
+        fluid_x = prepared_fluids[:, 0]
+        fluid_y = prepared_fluids[:, 1]
+        fluid_z = prepared_fluids[:, 2]
+        ax.scatter(fluid_x, fluid_z, fluid_y, c='black', marker='o')
 
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(0, 4)
+
 
     plt.show()
+# def o3d_visualize_prepared(path_to_pcd):
+#     # render box
+#     box_pcd = o3d.io.read_point_cloud(os.path.join(path_to_pcd, 'box.ply'))
+#     fluid_pcd = o3d.io.read_point_cloud(os.path.join(path_to_pcd, 'fluids.ply'))
+#     o3d.visualization.draw_geometries([box_pcd, fluid_pcd])
 
 
 if __name__ == '__main__':
     scene = '../settings/example_scene.json'
     with open(scene, 'r') as f:
         scene_obj = json.load(f)
-    box, box_normals, pos, vel = prepare_particles(scene_obj)
+    box, box_normals, pos, vel = prepare_particles(scene_obj, 'out_put')
+    print(np.max(box, axis=0))
+    print(np.min(box, axis=0))
+
+    print(np.max(pos, axis=0))
+    print(np.min(pos, axis=0))
+    mat_visualize_prepared(prepared_fluids=pos)
+
+    # o3d_visualize_prepared('../out_ply/out_put')
