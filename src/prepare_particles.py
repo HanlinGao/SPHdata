07639e11661d4073f5a5c6d82a1f4609571de839
Hanlin <hanlinGao@outlook.com>
2021-02-09 19:25:09 -0500

    3d initial data generation

---
+++ src/prepare_particles.py
@@ -0,0 +1,121 @@
+import open3d as o3d
+import numpy as np
+import tempfile
+import os
+import subprocess
+import json
+
+
+PARTICLE_RADIUS = 0.025
+VOLUME_SAMPLING_BIN = '../../SPlisHSPlasH/bin/VolumeSampling.exe'
+
+
+def numpy_from_bgeo(path):
+    import partio
+    p = partio.read(path)
+    print(path)
+    print(p)
+    pos = p.attributeInfo('position')
+    vel = p.attributeInfo('velocity')
+    ida = p.attributeInfo('trackid')  # old format
+    if ida is None:
+        ida = p.attributeInfo('id')  # new format after splishsplash update
+    n = p.numParticles()
+    pos_arr = np.empty((n, pos.count))
+    for i in range(n):
+        pos_arr[i] = p.get(pos, i)
+
+    vel_arr = None
+    if not vel is None:
+        vel_arr = np.empty((n, vel.count))
+        for i in range(n):
+            vel_arr[i] = p.get(vel, i)
+
+    if not ida is None:
+        id_arr = np.empty((n,), dtype=np.int64)
+        for i in range(n):
+            id_arr[i] = p.get(ida, i)[0]
+
+        s = np.argsort(id_arr)
+        result = [pos_arr[s]]
+        if not vel is None:
+            result.append(vel_arr[s])
+    else:
+        result = [pos_arr, vel_arr]
+
+    return tuple(result)
+
+
+def obj_surface_to_particles(objpath, radius=None):
+    if radius is None:
+        radius = PARTICLE_RADIUS
+    obj = o3d.io.read_triangle_mesh(objpath)
+    particle_area = np.pi * radius**2
+    # 1.9 to roughly match the number of points of SPlisHSPlasHs surface sampling
+    num_points = int(1.9 * obj.get_surface_area() / particle_area)
+    pcd = obj.sample_points_poisson_disk(num_points, use_triangle_normal=True)
+    points = np.asarray(pcd.points).astype(np.float32)
+    normals = -np.asarray(pcd.normals).astype(np.float32)
+    return points, normals
+
+
+def obj_volume_to_particles(objpath, scale=1, radius=None):
+    if radius is None:
+        radius = PARTICLE_RADIUS
+    with tempfile.TemporaryDirectory() as tmpdir:
+        outpath = os.path.join(tmpdir, 'out.bgeo')
+        scale_str = '{0}'.format(scale)
+        radius_str = str(radius)
+        status = subprocess.run([
+            VOLUME_SAMPLING_BIN, '-i', objpath, '-o', outpath, '-r', radius_str,
+            '-s', scale_str
+        ])
+        return numpy_from_bgeo(outpath)
+
+
+def prepare_particles(scene):
+    """generate initial data of box, box_normals, fluid_pos, fluid_vel according to the input json file
+
+    :param scene: set path to box.obj and fluid.obj, as well as the translation of positions
+    :return: numpy arrays of box, box_normals, fluid_pos, fluid_vel
+    """
+    # prepare static particles
+    walls = []
+    for x in scene['walls']:
+        points, normals = obj_surface_to_particles(x['path'])
+        if 'invert_normals' in x and x['invert_normals']:
+            normals = -normals
+        points += np.asarray([x['translation']], dtype=np.float32)
+        walls.append((points, normals))
+    box = np.concatenate([x[0] for x in walls], axis=0)
+    box_normals = np.concatenate([x[1] for x in walls], axis=0)
+
+    # prepare fluids
+    fluids = []
+    for x in scene['fluids']:
+        points = obj_volume_to_particles(x['path'])[0]
+        points += np.asarray([x['translation']], dtype=np.float32)
+        velocities = np.empty_like(points)
+        velocities[:, 0] = x['velocity'][0]
+        velocities[:, 1] = x['velocity'][1]
+        velocities[:, 2] = x['velocity'][2]
+        range_ = range(x['start'], x['stop'], x['step'])
+        fluids.append(
+            (points.astype(np.float32), velocities.astype(np.float32), range_))
+
+    pos = np.empty(shape=(0, 3), dtype=np.float32)
+    vel = np.empty_like(pos)
+    for points, velocities, range_ in fluids:
+        pos = np.concatenate([pos, points], axis=0)
+        vel = np.concatenate([vel, velocities], axis=0)
+
+    print(box)
+    print(pos)
+    print(vel)
+
+
+if __name__ == '__main__':
+    scene = '../settings/example_scene.json'
+    with open(scene, 'r') as f:
+        scene_obj = json.load(f)
+    prepare_particles(scene_obj)
